60abf79be13262986585affac81e812c5d19fc0a
SYED JALEES AKBAR SHAH <161203405+syedjalees@users.noreply.github.com>
2024-03-08 15:56:51 +0500

    Add files via upload

---
+++ Grade.js
@@ -0,0 +1,20 @@
+// Assignment: Find grades by marks/numbers
+var Marks = 89;
+if (Marks >= 90 && Marks <= 100) {
+    console.log("Grade A1");
+}
+else if (Marks >= 80 && Marks < 90) {
+    console.log("Grade A");
+}
+else if (Marks >= 60 && Marks < 80) {
+    console.log("Grade B");
+}
+else if (Marks >= 50 && Marks < 60) {
+    console.log("Grade C");
+}
+else if (Marks >= 40 && Marks < 50) {
+    console.log("Grade D");
+}
+else {
+    console.log("Fail");
+}
